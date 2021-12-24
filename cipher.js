8a5c9ff5831fe3237bd1c9d0c8c7da0d4c36ba7d
AngelicaJH <isc.florjardinez@gmail.com>
2021-12-23 23:17:35 -0600

    Updates

---
+++ cipher.js
@@ -0,0 +1,57 @@
+const cipher = {
+  offset: 0,
+  string: '',
+
+
+  encode: function (offset, string) {
+    const stringCifrada = [];
+    let nuevaPosicion = 0;
+
+    for (let i = 0; i <= string.length - 1; i++) { 
+
+      if ((string[i].charCodeAt() + offset) > 122) {
+
+        nuevaPosicion = (string[i].charCodeAt() + offset) % 91;
+        stringCifrada.push(String.fromCharCode(nuevaPosicion));
+
+      }
+      else {
+        nuevaPosicion = (string[i].charCodeAt() + offset);
+        stringCifrada.push(String.fromCharCode(nuevaPosicion));
+      }
+
+    }
+
+    return (stringCifrada.join(''));
+  },
+
+
+
+
+  decode: function (offset, string) {
+    const mensajeDescifrado = [];
+    let realPosicion;
+
+    for (let j = 0; j <= string.length - 1; j++) {
+
+      if (string[j].charCodeAt() - offset < 32) {
+        realPosicion = (string[j].charCodeAt() + 91) - offset;
+        mensajeDescifrado.push(String.fromCharCode(realPosicion));
+      }
+      if (string[j].charCodeAt() - offset <= 122 && (string[j].charCodeAt() - offset) >= 32) {
+
+        realPosicion = string[j].charCodeAt() - offset;
+        mensajeDescifrado.push(String.fromCharCode(realPosicion));
+      }
+
+
+    }
+
+    return (mensajeDescifrado.join(''));
+
+  }
+
+
+};
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,61 @@
+import cipher from './cipher.js';
+// import Swal from '../sweetalert2/dist/sweetalert2.js'
+
+//eventos de botones
+const btnCifrar = document.getElementById('cifrar');
+btnCifrar.addEventListener('click', function () {
+  //Datos 
+  cipher.offset = Number(document.getElementById('desplazar').value);
+  cipher.string = document.getElementById('texto').value;
+  document.getElementById('resultado').innerHTML= cipher.encode(cipher.offset, cipher.string);
+  
+  //modal
+  document.getElementsByClassName("fondoTransparente")[0].style.display="block";
+});
+
+const btnDescifrar = document.getElementById('descifrar');
+btnDescifrar.addEventListener('click', function () {
+  //Datos 
+  cipher.offset = Number(document.getElementById('desplazar').value);
+  cipher.string = document.getElementById('texto').value;
+  document.getElementById('resultadoDescifrado').innerHTML = cipher.decode(cipher.offset, cipher.string);
+  //modal
+  document.getElementsByClassName("fondoTransparente")[1].style.display="block";
+});
+
+
+//btn limpiar
+const btnLimpiar = document.getElementById('limpiar');
+btnLimpiar.addEventListener('click', function () {
+  document.getElementById('texto').value="";
+});
+
+//Btn cerrar de modal
+document.getElementsByClassName("modal_cerrar")[0].addEventListener('click', function()
+{document.getElementsByClassName("fondoTransparente")[0].style.display="none"});
+
+document.getElementsByClassName("modal_cerrar")[1].addEventListener('click', function()
+{document.getElementsByClassName("fondoTransparente")[1].style.display="none"});
+
+//btn copiar
+const btnCopiar = document.getElementById('copiarResultado');
+btnCopiar.addEventListener('click',function(){
+  const copiar= document.getElementById('resultado');
+  copiar.select();
+  document.execCommand('copy');
+});
+
+const btnCopiarDescifrado = document.getElementById('copiarDescifrado');
+btnCopiarDescifrado.addEventListener('click',function(){
+  const copia= document.getElementById('resultadoDescifrado');
+  copia.select();
+  document.execCommand('copy');
+});
+
+
+const btnInfo=document.getElementById('mensaje');
+btnInfo.addEventListener('click',function(){
+  
+  Swal.fire({text:'Hola, esta es herramienta enfocada a ayudar cifrar mensajes para guardabosques quienes tienen como una de sus tareas el monitoreo biológico de las especies que habitan en un área asignada.'})
+ 
+})
